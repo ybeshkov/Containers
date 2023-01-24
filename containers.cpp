32dd98ff8d7243557444106149915f2a981a3753
ybeshkov <91119020+ybeshkov@users.noreply.github.com>
2023-01-24 23:32:33 +0300

    Add files via upload

---
+++ containers.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <iterator>
+#include <algorithm>
+#include <numeric>
+
+using namespace std;
+
+void show(const vector<int>& p) {
+    cout << "Vector: ";
+    for (int i : p)
+        cout << i << " ";
+    cout << "\n" << endl;
+}
+
+int main() {
+    vector<int> a = { 13, 92, 4, 546, 54, 7, 666, 123, 45, 1, 69 };
+    int k = 9;
+    int count_k = 0;
+    show(a);
+
+    for (int i = 0; i < size(a); i++) {
+        if (a[i] % k == 0) count_k = count_k + 1;
+    }
+    cout << count_k;
+    return 0;
+}
+
